826a1cbaedf05896b3295664b2af73b04863b44a
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-07-27 00:34:46 -0700

    update seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,33 @@
-puts "---------- Gift Givens Engine ------------"
 user = Erp::User.first
-contact = Erp::Contacts::Contact.first
-product = Erp::Products::Product.first
+contacts = ['Marcus Doe', 'Nick Larson', 'Richard Stone', 'Paul Kiton', 'Mark	Otto']
 
-puts "Create sample gift given"
-Erp::GiftGivens::Given.where(code: "GG001").destroy_all
-given = Erp::GiftGivens::Given.create(
-  code: "GG001",
-  given_date: Time.now,
-  contact_id: contact.id,
-  creator_id: user.id
-)
-puts given.errors.to_json if !given.errors.empty?
+# Contacts
+contacts.each do |c|
+  Erp::Contacts::Contact.where(name: c).destroy_all
+  num = Erp::Contacts::Contact.all.order('id DESC').first.id
+  Erp::Contacts::Contact.create(
+    contact_type: Erp::Contacts::Contact::TYPE_PERSON,
+    name: c,
+    code: 'CUS'+ num.to_s.rjust(3, '0'),
+    creator_id: user.id
+  )
+end
 
-puts "Create sample gift given detail"
-given_detail = Erp::GiftGivens::GivenDetail.create(
-  product_id: product.id,
-  given_id: given.id,
-  quantity: 2
-)
-puts given_detail.errors.to_json if !given_detail.errors.empty?
+
+Erp::GiftGivens::Given.all.destroy_all
+(1..20).each do |num|
+  contact_ps = Erp::Contacts::Contact.where(contact_type: Erp::Contacts::Contact::TYPE_PERSON).order("RANDOM()").first
+  given = Erp::GiftGivens::Given.create(
+    code: 'GG'+ num.to_s.rjust(3, '0'),
+    given_date: Time.now,
+    contact_id: contact_ps.id,
+    creator_id: user.id
+  )
+  Erp::Products::Product.where(id: Erp::Products::Product.pluck(:id).sample(rand(1..5))).each do |product|
+    Erp::GiftGivens::GivenDetail.create(
+      product_id: product.id,
+      given_id: given.id,
+      quantity: rand(1..3)
+    )
+  end
+end
