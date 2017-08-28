88b2e68dc99b51015ba37c94c116cf431c5657b1
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-08-28 02:20:00 -0700

    gift givens ability update (set status with cancan)

---
+++ app/decorators/models/ability_decorator.rb
@@ -0,0 +1,24 @@
+Erp::Ability.class_eval do
+  def gift_givens_ability(user)
+    
+    can :activate, Erp::GiftGivens::Given do |given|
+      given.is_draft? or given.is_deleted?
+    end
+    
+    can :delivery, Erp::GiftGivens::Given do |given|
+      given.is_draft? or given.is_active?
+    end
+    
+    can :delete, Erp::GiftGivens::Given do |given|
+      given.is_draft? or given.is_active? or given.is_delivered?
+    end
+    
+    can :update, Erp::GiftGivens::Given do |given|
+      given.is_draft? or given.is_active? or given.is_delivered?
+    end
+    
+    can :export_file, Erp::GiftGivens::Given do |given|
+      given.is_draft? or given.is_active? or given.is_delivered?
+    end
+  end
+end
