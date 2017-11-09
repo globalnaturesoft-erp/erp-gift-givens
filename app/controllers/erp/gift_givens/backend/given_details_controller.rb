034d3534563cb485ae89a542f4a7035cf1d46ee9
Luan Pham <luanpm88@gmail.com>
2017-11-08 18:43:28 -0800

    update detail form

--- app/controllers/erp/gift_givens/backend/given_details_controller.rb
+++ app/controllers/erp/gift_givens/backend/given_details_controller.rb
@@ -5,7 +5,8 @@ module Erp
         def given_line_form
           @given_detail = GivenDetail.new
           @given_detail.product_id = params[:add_value]
-          
+          @given_detail.state = Erp::Products::State.first
+
           render partial: params[:partial], locals: {
             given_detail: @given_detail,
             uid: helpers.unique_id()
