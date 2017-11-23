b1be8fe824eee62822fd5222b26ee458499e0879
Luan Pham <luanpm88@gmail.com>
2017-11-23 16:37:27 +0700

    update gift givens layout

--- app/models/erp/gift_givens/given.rb
+++ app/models/erp/gift_givens/given.rb
@@ -4,30 +4,30 @@ module Erp::GiftGivens
     belongs_to :contact, class_name: "Erp::Contacts::Contact", foreign_key: :contact_id
     has_many :given_details, inverse_of: :given, dependent: :destroy
     accepts_nested_attributes_for :given_details, :reject_if => lambda { |a| a[:product_id].blank? || a[:warehouse_id].blank? || a[:quantity].blank? || a[:quantity].to_i <= 0 }, :allow_destroy => true
-    
+
     after_save :update_cache_products_count
-    
+
     validates :code, uniqueness: true
     validates :given_date, :contact_id, presence: true
-    
+
     # class const
     STATUS_DRAFT = 'draft'
     STATUS_ACTIVE = 'active'
     STATUS_DELIVERED = 'delivered'
     STATUS_DELETED = 'deleted'
-    
+
     # Filters
     def self.filter(query, params)
       params = params.to_unsafe_hash
-      
+
       # join with users table for search creator
       query = query.joins(:creator)
-      
+
       # join with contacts table for search contact
       query = query.joins(:contact)
-      
+
       and_conds = []
-      
+
       #filters
       if params["filters"].present?
         params["filters"].each do |ft|
@@ -38,7 +38,7 @@ module Erp::GiftGivens
           and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
         end
       end
-      
+
       #keywords
       if params["keywords"].present?
         params["keywords"].each do |kw|
@@ -49,44 +49,61 @@ module Erp::GiftGivens
           and_conds << '('+or_conds.join(' OR ')+')'
         end
       end
-      
+
       # add conditions to query
       query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
-      
+
       # search by a given date
       # if params[:date].present?
 			# 	date = params[:date].to_date
 			# 	query = query.where("given_date >= ? AND given_date <= ?", date.beginning_of_day, date.end_of_day)
 			# end
-			
+
 			# global filters
       global_filter = params[:global_filter]
-      
+
 			if global_filter.present?
-				
+
+        # if has period
+        if global_filter[:period].present?
+          period = Erp::Periods::Period.find(global_filter[:period])
+          global_filter[:from_date] = period.from_date
+          global_filter[:to_date] = period.to_date
+        end
+
+				# filter by order from date
+				if global_filter[:from_date].present?
+					query = query.where('given_date >= ?', global_filter[:from_date].to_date.beginning_of_day)
+				end
+
+				# filter by order to date
+				if global_filter[:to_date].present?
+					query = query.where('given_date <= ?', global_filter[:to_date].to_date.end_of_day)
+				end
+
 				# filter by given from date
 				if global_filter[:given_from_date].present?
 					query = query.where('given_date >= ?', global_filter[:given_from_date].to_date.beginning_of_day)
 				end
-				
+
 				# filter by given to date
 				if global_filter[:given_to_date].present?
 					query = query.where('given_date <= ?', global_filter[:given_to_date].to_date.end_of_day)
 				end
-				
+
 				# filter by contact
 				if global_filter[:contact].present?
 					query = query.where(contact_id: global_filter[:contact])
 				end
 			end
-      
+
       return query
     end
-    
+
     def self.search(params)
       query = self.all
       query = self.filter(query, params)
-      
+
       # order
       if params[:sort_by].present?
         order = params[:sort_by]
@@ -96,64 +113,64 @@ module Erp::GiftGivens
       else
 				query = query.order('created_at desc')
       end
-      
+
       return query
     end
-    
+
     # display creator name
     def creator_name
       creator.present? ? creator.name : ''
     end
-    
+
     # Set status for stock transfer
     def set_draft
       update_attributes(status: Erp::StockTransfers::Transfer::STATUS_DRAFT)
     end
-    
+
     def set_activate
       update_attributes(status: Erp::StockTransfers::Transfer::STATUS_ACTIVE)
     end
-    
+
     def set_delivery
       update_attributes(status: Erp::StockTransfers::Transfer::STATUS_DELIVERED)
     end
-    
+
     def set_delete
       update_attributes(status: Erp::StockTransfers::Transfer::STATUS_DELETED)
     end
-    
+
     # check if gift given is draft/active/delivered/deleted
     def is_draft?
       return self.status == Erp::GiftGivens::Given::STATUS_DRAFT
     end
-    
+
     def is_active?
       return self.status == Erp::GiftGivens::Given::STATUS_ACTIVE
     end
-    
+
     def is_delivered?
       return self.status == Erp::GiftGivens::Given::STATUS_DELIVERED
     end
-    
+
     def is_deleted?
       return self.status == Erp::GiftGivens::Given::STATUS_DELETED
     end
-    
+
     # display contact name
     def contact_name
       contact.present? ? contact.name : ''
     end
-    
+
     # Total item count for transfer details
     def total_quantity
 			return given_details.sum('quantity')
 		end
-    
+
     # Update cache products count
     def update_cache_products_count
 			self.update_column(:cache_products_count, self.total_quantity)
 		end
-    
+
     # Generate code
     before_validation :generate_code
     def generate_code
